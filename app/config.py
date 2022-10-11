1a7102beac1e9d7c9c809cb65d9e11af3c5ff5e1
TrttiTrttu <trtti.trttu@gmail.com>
2022-10-11 13:07:39 +0300

    edit config file for automatic build

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-database = os.getenv("DATABASE")
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+database = os.getenv("MYSQL_DATABASE")
 
 SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{user}:{password}@database/{database}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
