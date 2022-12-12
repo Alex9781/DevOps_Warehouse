ed2cbaa8a9215604cb08c82fcfa03119552a74f8
Unidentified Squirrel <alex.zmiev.az@gmail.com>
2022-12-12 21:13:30 +0300

    fix config file

--- app/config.py
+++ app/config.py
@@ -6,5 +6,5 @@ database = os.getenv("MYSQL_DATABASE")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{user}:{password}@127.0.0.1/{database}"
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{user}:{password}@database/{database}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
