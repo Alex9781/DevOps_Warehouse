966f77f8d903a1ced1fc3ff93512aed7120a39ef
Unidentified Squirrel <alex.zmiev.az@gmail.com>
2022-12-12 23:14:46 +0300

    moved database utl to env file

--- app/config.py
+++ app/config.py
@@ -3,8 +3,9 @@ import os
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 database = os.getenv("MYSQL_DATABASE")
+database_url = os.getenv("DATABASE_URL")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{user}:{password}@database/{database}"
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{user}:{password}@{database_url}/{database}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
